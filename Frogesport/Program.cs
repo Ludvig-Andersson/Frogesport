f7c64fa64571d483ec8795652f0b93a4bb5e277c
Ludvig-Andersson <Ludvig.andersson@elev.ga.ntig.se>
2020-09-22 15:30:55 +0200

    klar

--- Frogesport/Program.cs
+++ Frogesport/Program.cs
@@ -6,8 +6,8 @@ namespace Frogesport
     {
         static void Main(string[] args)
         {
-            int points = 0;
-            int health = 5;
+
+            int health = 3;
 
             System.Console.WriteLine("Vill du börja Frågesporten? (ja eller nej)");
 
@@ -16,33 +16,99 @@ namespace Frogesport
             if (fråga1 == "ja")
             {
                 Console.WriteLine("Då startar vi!!");
+                Console.Clear();
 
             }
 
             else
             {
-
+                System.Console.WriteLine("Stäng ner skiten då!!");
             }
-
+            Console.WriteLine("health:" + health);
             Console.WriteLine("Vad heter du?");
-            System.Console.WriteLine("A. Herbert");
-            System.Console.WriteLine("B. Albertina");
-            System.Console.WriteLine("C. Ludvig");
+            System.Console.WriteLine("a. Herbert");
+            System.Console.WriteLine("b. Albertina");
+            System.Console.WriteLine("c. Ludvig");
 
             string svar1 = Console.ReadLine();
 
-            if (svar1 == "C")
+            if (svar1 == "c")
             {
 
-                points++;
                 System.Console.WriteLine("Grattis du hade rätt");
+                Console.ReadLine();
             }
 
             else
             {
                 health = health - 1;
-                System.Console.WriteLine("Det var fel!");
 
+                System.Console.WriteLine("Det var fel!");
+                Console.ReadLine();
+
+            }
+            Console.Clear();
+            Console.WriteLine("health:" + health);
+            Console.WriteLine("Vad är världens farligaste djur?");
+            System.Console.WriteLine("a. Flodhäst");
+            System.Console.WriteLine("b. Lejon");
+            System.Console.WriteLine("c. Elefant");
+
+            string svar2 = Console.ReadLine();
+
+            if (svar2 == "a")
+            {
+
+
+                System.Console.WriteLine("Grattis du hade rätt");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                health = health - 1;
+
+                System.Console.WriteLine("Det var fel!");
+                Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine("health:" + health);
+            Console.WriteLine("Om man gräver ett 6 meter djupt hål, hur djupt är hålet?");
+            System.Console.WriteLine("a. 20 meter");
+            System.Console.WriteLine("b. 35 meter");
+            System.Console.WriteLine("c. 6 meter");
+
+            string svar3 = Console.ReadLine();
+
+            if (svar3 == "c")
+            {
+
+
+                System.Console.WriteLine("Grattis du hade rätt");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                health = health - 1;
+
+                System.Console.WriteLine("Det var fel!");
+                Console.ReadLine();
+            }
+
+            if (health == 0)
+            {
+                Console.Clear();
+                System.Console.WriteLine("Ajj du förlorade, bättre lycka nästa gång!!");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.Clear();
+                System.Console.WriteLine("Snyggt du vann och hade (" + health + ") health kvar!!");
+                Console.ReadLine();
             }
 
             Console.ReadLine();
